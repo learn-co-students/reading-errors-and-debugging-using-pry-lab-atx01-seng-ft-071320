586a3f1da486ffc2a35a62226f3e32fa4dd72f35
Jimislimdrix <sethsjjones@gmail.com>
2020-06-26 15:36:44 -0500

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
-# don't forget to add: require 'pry'
+
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    new_string = "ssssssssss" + string
+    return new_string
   else
     string
   end
 end
+
--- spec/pemdos_spec.rb
+++ spec/pemdos_spec.rb
@@ -1,3 +1,5 @@
+
+
 require_relative '../fix_using_tests/pemdos'
 
 describe "pemdos" do
